3b3bda960541e94a339ed62df7a723423eb46131
Tech Luminaire <info@techluminaire.co.uk>
2019-10-30 21:58:17 +0000

    Added Android Splash screen

---
+++ Android/SplashActivity.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Util;
+
+using Xamarin.Forms.Platform.Android;
+using Android.Content.PM;
+
+
+namespace okKindredXamarin.Android
+{
+    [Activity(Theme = "@style/MainTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : AppCompatActivity
+    {
+        static readonly string TAG = "X:" + typeof(SplashActivity).Name;
+
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+            Log.Debug(TAG, "SplashActivity.OnCreate");
+        }
+
+        // Launches the startup task
+        protected override void OnResume()
+        {
+            base.OnResume();
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+
+        // Prevent the back button from canceling the startup process
+        public override void OnBackPressed() { }
+    }
+
+
+}
+
